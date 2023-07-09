439a49684e821bf679dcc75f311c9fe69a3f0f92
Pedro&Dev <pvcg2003@gmail.com>
2023-07-09 14:27:29 -0300

    Update bancary_system.py

--- bancary_system.py
+++ bancary_system.py
@@ -53,7 +53,7 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
 
 
 def exibir_extrato(saldo, /, *, extrato):
-    print("\n================ EXTRATO ================")
+    print("\n---------------- EXTRATO ----------------")
     print("Não foram realizadas movimentações." if not extrato else extrato)
     print(f"\nSaldo:\t\tR$ {saldo:.2f}")
     print("==========================================")
